0a03467de9ee0254499e42adff91654063646e16
Chukwudi <cchinwenmeri@gmail.com>
2026-03-25 12:01:18 +0100

    dist changes for production

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/altschool_semester2_assessment1/'
-})
+  base: '/altschool_semester2_assessment1/',
+});
